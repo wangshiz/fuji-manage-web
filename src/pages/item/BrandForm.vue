058a523fd453ee228a801e940b9121522deeb891
Wsz_ <312406078@qq.com>
2020-01-08 21:30:06 +0800

    fix brandform

--- src/pages/item/BrandForm.vue
+++ src/pages/item/BrandForm.vue
@@ -72,7 +72,7 @@
           this.$http({
             method: this.isEdit ? 'put' : 'post',
             url: '/item/brand',
-            data: this.$qs.stringfy(params)
+            data: this.$qs.stringify(params)
           }).then(() => {
             // 关闭窗口
             this.$emit("close");
